f9ec9db5ab5a92b902201ae6020cc56d5387ae4e
parthshah <parthshah@10.189.158.216>
2015-02-17 00:26:53 -0800

    Files Committed.

--- BaaS/src/main/java/com/sjsu/userManager/ApplicationManagement.java
+++ BaaS/src/main/java/com/sjsu/userManager/ApplicationManagement.java
@@ -1,8 +1,6 @@
 package com.sjsu.userManager;
 
 public class ApplicationManagement {
-	
-	//App Class From bhavi.
-	//One thing added.
+
 
 }
--- BaaS/src/main/java/com/sjsu/userManager/UserManagement.java
+++ BaaS/src/main/java/com/sjsu/userManager/UserManagement.java
@@ -1,13 +1,5 @@
 package com.sjsu.userManager;
 
 public class UserManagement {
-	//bhavi joshi.
-	//ParthAdds..FastForwardAdded.
-	//nasdfsdkfdskfj
-	//asdfsadfkjkdsafjkasjfkasjparthbhavi.
-	//jjjjjj
-	//Parth...
-	//Shah.
-	//Herare adskfjsdkfj adfksdajf
-	// second line added.
+
 }
